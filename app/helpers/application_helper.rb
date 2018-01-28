0b0b68597e34657a509fc3bae6e4c1a0edade468
Miroslav Poul <miroslav@poul.sk>
2018-01-28 15:21:55 +0100

    First version

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def active_link_to (text, href, class_name ="")
+    if current_page?(href) == true
+      "<li class='active'><a href='#{href}' class='#{class_name}'>#{text}<span class='sr-only'>(current)</span></a></li>".html_safe
+    else
+      "<li><a href='#{href}' class='#{class_name}'>#{text}</a></li>".html_safe
+    end
+
+  end
 end
